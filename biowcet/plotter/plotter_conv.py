f33491f52b22f7ecdf3f00e6b474a371da5f8b5a
Iraline <iraline@hotmail.com>
2023-12-06 08:01:52 -0300

    Add plotter

---
+++ biowcet/plotter/plotter_conv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import sys
+
+def plotar_grafico(nome_arquivo):
+    with open(nome_arquivo, 'r') as arquivo:
+        # Lendo os valores do arquivo e removendo espaços em branco e quebras de linha
+        valores = [int(line.strip()) for line in arquivo.readlines()]
+        for i in range(0, len(valores),1):
+            plt.axvline(x=i, color='red', linestyle='dashed', linewidth=1)
+        # Plotando os valores em um gráfico de linha
+        plt.plot(valores)
+        plt.title('WCETS')
+        plt.xlabel('ants')
+        plt.ylabel('wcet parcial')
+        plt.grid(True)
+        plt.show()
+
+# Chamando a função com o nome do arquivo que contém os valores
+if(len(sys.argv) > 1):
+    func = sys.argv[1]
+path_wcet = '../biowcet/din/build/wcets_'+ func + '.txt'  # Caminho para o arquivo DOT
+plotar_grafico(path_wcet)
